00e8782642463bda147461f71fca4913fcecbe87
TerryYFF <terryyan47@gmail.com>
2019-03-23 16:23:41 -0400

    updated GUI & modified data types for word and guess

--- hangman.pl
+++ hangman.pl
@@ -6,19 +6,33 @@ my @dict;
 my $length;
 my $word_index;
 my $word;
-my @word_list;
 my $word_length;
 my $guess;
-my @guess_list;
 my $letter;
 my $i;
 my $count = 0;
 my $match = 0;
 my $win = 0;
 my @letters_guessed;
-our @words;
-my $guessed;
-my $one;
+
+#Hangman
+#
+# Display punctuation in game.  _ _ _ _ ' _ (Fixed)
+# Case Insensitive. A, a should be the same. (Fixed)
+
+
+# Ideas for creative project
+#
+# Processing.
+# EG. When it has this letter at the end of the string, it does this etc.
+# Variable number of arguments to a function.
+# Dereferencing
+# Hashing
+# Dynamic Arrays
+# Glue Things together
+# Variable number arguments
+# Text Processing
+# regular expression parsing.
 
 
 
@@ -28,107 +42,137 @@ my $one;
 # https://www.perl.com/pub/2001/03/gui.html/
 # https://docstore.mik.ua/orelly/perl3/tk/ch01_04.htm
 
-my $nmbr = 1;
-
-my $mw = MainWindow->new;
+my $mw;
 my $game;
-$mw->geometry("500x500");
-$mw->title("Hangman!!!");
+my $GuessList;
+my $LettersGuessed;
 
-$mw->Label(-text => 'Do you want to play hangman?')->pack();
-
-my $button1 = $mw->Button(-text => "Yes", -command => \&button1_sub)->pack(); #Yes Button.
-my $button2 = $mw->Button(-text => "No", -command => \&button2_sub)->pack(); #No Button.
+# sub GUI {
+# 	$mw = MainWindow->new;
+# 	$mw->geometry("500x500");
+# 	$mw->title("Hangman!!!");
+#
+# 	$mw->Label(-text => 'Do you want to play hangman?')->pack();
+#
+# 	my $button1 = $mw->Button(-text => "Yes", -command => \&button1_sub)->pack(); #Yes Button.
+# 	my $button2 = $mw->Button(-text => "No", -command => \&button2_sub)->pack(); #No Button.
+# }
 
 
 # Window that pops up after you press Yes
-sub button1_sub {
+sub GUI {
 	$game = MainWindow -> new;
-	$game->title("Sub Window #1");
 	$game->geometry("500x500");
 	$game->title("Hangman");
-	$game->Label(-text => "@guess_list")->pack();
-	$game->Label(-text => "These are the letters you have guessed: @letters_guessed")->pack();
-	$game->Label(-text => "Guess the Letter")->pack();
-	$game->Entry(-background => 'black', -foreground => 'white')->pack(-side => "top");
-	my $A = $game->Button(-text => "A", -command => \&letterA)->pack();
+	$GuessList = $game->Label(-text => "$guess")->pack();
+	$LettersGuessed = $game->Label(-text => "Letters Guessed: @letters_guessed")->pack();
+	my $A = $game->Button(-text => "A", -command => [\&letter, 'a'])->pack(-side => 'left');
+	my $B = $game->Button(-text => "B", -command => [\&letter, 'b'])->pack(-side => 'left');
+	my $C = $game->Button(-text => "C", -command => [\&letter, 'c'])->pack(-side => 'left');
+	my $D = $game->Button(-text => "D", -command => [\&letter, 'd'])->pack(-side => 'left');
+	my $E = $game->Button(-text => "E", -command => [\&letter, 'e'])->pack(-side => 'left');
 }
 
 
-# Window that pops after you press No
-sub button2_sub {
-  my $yesno_button = $mw->messageBox(-message => "Are you sure you don't want to play?",
-                                        -type => "yesno", -icon => "question");
+# # Window that pops after you press No
+# sub button2_sub {
+#   my $yesno_button = $mw->messageBox(-message => "Are you sure you don't want to play?",
+#                                         -type => "yesno", -icon => "question");
+#
+#   if ($yesno_button eq "Yes") {
+#     $mw->messageBox(-message => "See you next time.", -type => "ok");
+#     exit;
+#   } else {
+#     $mw->messageBox(-message => "I didn't think so either.", -type => "ok");
+#   }
+# }
 
-  if ($yesno_button eq "Yes") {
-    $mw->messageBox(-message => "See you next time.", -type => "ok");
-    exit;
-  } else {
-    $mw->messageBox(-message => "I didn't think so either.", -type => "ok");
-  }
+# Letter Button takes a letter as a parameter
+sub letter{
+	$letter = $_[0];
+	&CheckLetter();
 }
 
-# Letter A button
-sub letterA{
-	@letters_guessed = "a";
+sub CheckLetter {
+	if (&check_guessed($letter, join("", @letters_guessed))){
+		$LettersGuessed -> configure(-text => "Letters Guessed: @letters_guessed");
+		$GuessList -> configure(-text => "$guess");
+	} else {
+		push(@letters_guessed, $letter);
+		@letters_guessed = sort(@letters_guessed);
+		$LettersGuessed -> configure(-text => "Letters Guessed: @letters_guessed");
+		for ($i = 0; $i < $word_length; $i++){
+			if ($letter eq substr($word, $i, 1)){
+				substr($guess, $i, 1, $letter);
+				$match = 1;
+			} elsif (uc($letter) eq substr($word, $i, 1)) {
+				substr($guess, $i, 1, uc($letter));
+				$match = 1;
+			}
+		}
+		$GuessList -> configure(-text => "$guess");
+		if ($match == 0) {
+			$count++;
+		}
+		$match = 0;
+		if ($word eq $guess){
+			$win = 1;
+		}
+	}
 }
 
 
 #! /usr/bin/perl
 # Initiate the dictionary
-
-#@dict = &init_dict;
-#$length = @dict;
-#print("The length of the dictionary is ", $length, " words. \n");
-
-my $size = `wc -l dictionary.txt`;
-$size =~ s/\D//g;
+#my $size = `wc -l dictionary.txt`;
+my $size = 20;
+#$size =~ s/\D//g;
 print("The size of the file is $size\n");
 
 # Choose the target word
 $word_index = int(rand($size));
-#$word = @dict[$word_index];
+
 $word = &fetch_word($word_index);
 chop($word);
 chop($word);
-@word_list = split(//, $word);
-$word_length = @word_list;
-#print("The target word is ", @word_list, "\n");
-
+$word_length = length($word);
 $guess = "_" x $word_length;
-@guess_list = split(//, $guess);
-print(@guess_list, "\n");
-
-
-
-until ($count == 6 || $win == 1) {
-	&hangmanDisplay();
-	@letters_guessed = sort(@letters_guessed);
-	print("These are the letters you have guessed: @letters_guessed\n");
-	print("Please pick a letter: ");
-	$letter = <STDIN>;
-	print("\n");
-	chop($letter);
-	if (&check_guessed($letter, join("", @letters_guessed))){
-		print("The letter you entered had been guessed before.\n")
-	} else {
-		push(@letters_guessed, $letter);
-		for ($i = 0; $i < $word_length; $i++){
-			if ($letter eq $word_list[$i]){
-				splice(@guess_list, $i, 1, $letter);
-				$match = 1;
-			}
-		}
-		print(@guess_list, "\n");
-		if ($match == 0) {
-			$count++;
-		}
-		$match = 0;
-		if ($word eq join("", @guess_list)){
-			$win = 1;
-		}
-	}
+if (index($word, "'") != -1){
+	substr($guess, index($word, "'"), 1, "'");
 }
+print($guess, "\n");
+
+
+
+&GUI();
+
+# until ($count == 6 || $win == 1) {
+# 	&hangmanDisplay();
+# 	print("These are the letters you have guessed: @letters_guessed\n");
+# 	print("Please pick a letter: ");
+# 	# $letter = <STDIN>;
+# 	# print("\n");
+# 	# chop($letter);
+# 	if (&check_guessed($letter, join("", @letters_guessed))){
+# 		$game->Label(-text => "These are the letters you have guessed: @letters_guessed")->pack();
+# 	} else {
+# 		push(@letters_guessed, $letter);
+# 		for ($i = 0; $i < $word_length; $i++){
+# 			if ($letter eq $word_list[$i]){
+# 				splice(@guess_list, $i, 1, $letter);
+# 				$match = 1;
+# 			}
+# 		}
+# 		print(@guess_list, "\n");
+# 		if ($match == 0) {
+# 			$count++;
+# 		}
+# 		$match = 0;
+# 		if ($word eq join("", @guess_list)){
+# 			$win = 1;
+# 		}
+# 	}
+# }
 
 if ($win == 1){
 	print("You win!\n");
@@ -149,24 +193,12 @@ sub fetch_word {
 	$target;
 }
 
-# Intialize the dictionary
-sub init_dict {
-	local(@words);
-	open(DICT, "test.txt");
-#	open (DICT, "dictionary.txt");
-	while ($word = <DICT>) {
-		chop($word);
-		@words = (@words, $word);
-	}
-	close (DICT);
-	@words;
-}
 
 # Check if a letter has already been guessed
 sub check_guessed {
 	@letters_guessed = split(//, $_[1]);
-	$one = @letters_guessed;
-	$guessed = 0;
+	my $one = @letters_guessed;
+	my $guessed = 0;
 	for ($i = 0; $i < $one; $i++) {
 		if ($_[0] eq $letters_guessed[$i]) {
 			$guessed = 1;
@@ -275,35 +307,3 @@ sub hangman7Display {
 
 	MainLoop;
 
-
-#This is not implemented. This only runs through dictionary and reads how many lines, charaters, and words there are.
-
-
-
-open(FILE, "dictionary.txt") or die "Could not open file: $!";
-
-my ($lines, $words, $chars) = (0,0,0);
-
-while (<FILE>) {
-    $lines++;
-    $chars += length($_);
-    $words += scalar(split(/\s+/, $_));
-}
-
-print("lines=$lines words=$words chars=$chars\n");
-
-
-open(FILE, "dictionary.txt") or die "Could not open file: $!";
-
-my @Checking = <FILE>;
-close FILE;
-print ("lines = $lines\n");
-my $random = (rand($lines));
-print ("Random number = $random\n");
-print $Checking[$random];
-
-# https://stackoverflow.com/questions/782087/how-do-i-count-the-characters-words-and-lines-in-a-file-using-perl
-
-
-#hashtable
-#tree
