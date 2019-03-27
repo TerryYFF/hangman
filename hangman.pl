f6cc1dafa45ee0ea6a73db20f7c69fc636d610b9
Itaru <itaru.liverpool@gmail.com>
2019-03-27 14:17:59 -0400

    Updated drawing the Hangman.

--- hangman.pl
+++ hangman.pl
@@ -10,30 +10,10 @@ my $word_length;
 my $guess;
 my $letter;
 my $i;
-my $count = 1;
+my $count = 0;
 my $match = 0;
 my $win = 0;
 my @letters_guessed;
-
-#Hangman
-#
-# Display punctuation in game.  _ _ _ _ ' _ (Fixed)
-# Case Insensitive. A, a should be the same. (Fixed)
-
-
-# Ideas for creative project
-#
-# Processing.
-# EG. When it has this letter at the end of the string, it does this etc.
-# Variable number of arguments to a function.
-# Dereferencing
-# Hashing
-# Dynamic Arrays
-# Glue Things together
-# Variable number arguments
-# Text Processing
-# regular expression parsing.
-
 
 
 #GUI Window. Must have TK.
@@ -42,75 +22,71 @@ my @letters_guessed;
 # https://www.perl.com/pub/2001/03/gui.html/
 # https://docstore.mik.ua/orelly/perl3/tk/ch01_04.htm
 
-my $game;
+
+my $mw;
 my $GuessList;
 my $LettersGuessed;
 my $hangmanDisplay;
+my $DisplayHangman;
+my $canvas;
 
-# sub GUI {
-# 	$mw = MainWindow->new;
-# 	$mw->geometry("500x500");
-# 	$mw->title("Hangman!!!");
-#
-# 	$mw->Label(-text => 'Do you want to play hangman?')->pack();
-#
-# 	my $button1 = $mw->Button(-text => "Yes", -command => \&button1_sub)->pack(); #Yes Button.
-# 	my $button2 = $mw->Button(-text => "No", -command => \&button2_sub)->pack(); #No Button.
-# }
+$mw = MainWindow -> new;
+$mw->geometry("500x500");
+$mw->title("Hangman");
 
 
-# Window that pops up after you press Yes
+#! /usr/bin/perl
+# Initiate the dictionary
+# my $size = `wc -l dictionary.txt`;
+my $size = 20;
+#$size =~ s/\D//g;
+$mw -> Label(-text => "The size of the file is $size")->pack();
+
 sub GUI {
-	$game = MainWindow -> new;
-	$game->geometry("500x500");
-	$game->title("Hangman");
-	$GuessList = $game->Label(-text => "$guess")->pack();
-	my $canvas = $game->Canvas(-relief => "sunken", -background => "blue");
-	$canvas->createOval(30, 80, 100, 150, -fill => "red");
+	$LettersGuessed = $mw->Label(-text => "Letters Guessed: @letters_guessed")->pack();
 
-	$canvas -> pack();
-	$LettersGuessed = $game->Label(-text => "Letters Guessed: @letters_guessed")->pack();
-	my $A = $game->Button(-text => "A", -command => [\&letter, 'a'])->pack(-side => 'left');
-	my $B = $game->Button(-text => "B", -command => [\&letter, 'b'])->pack(-side => 'left');
-	my $C = $game->Button(-text => "C", -command => [\&letter, 'c'])->pack(-side => 'left');
-	my $D = $game->Button(-text => "D", -command => [\&letter, 'd'])->pack(-side => 'left');
-	my $E = $game->Button(-text => "E", -command => [\&letter, 'e'])->pack(-side => 'left');
-	my $F = $game->Button(-text => "F", -command => [\&letter, 'f'])->pack(-side => 'left');
-	my $G = $game->Button(-text => "G", -command => [\&letter, 'g'])->pack(-side => 'left');
-	my $H = $game->Button(-text => "H", -command => [\&letter, 'h'])->pack(-side => 'left');
-	my $I = $game->Button(-text => "I", -command => [\&letter, 'i'])->pack(-side => 'left');
-	my $J = $game->Button(-text => "J", -command => [\&letter, 'j'])->pack(-side => 'left');
-	my $K = $game->Button(-text => "K", -command => [\&letter, 'k'])->pack(-side => 'left');
-	my $L = $game->Button(-text => "L", -command => [\&letter, 'l'])->pack(-side => 'left');
-	my $M = $game->Button(-text => "M", -command => [\&letter, 'm'])->pack(-side => 'left');
-	my $N = $game->Button(-text => "N", -command => [\&letter, 'n'])->pack(-side => 'left');
-	my $O = $game->Button(-text => "O", -command => [\&letter, 'o'])->pack(-side => 'left');
-	my $P = $game->Button(-text => "P", -command => [\&letter, 'p'])->pack(-side => 'left');
-	my $Q = $game->Button(-text => "Q", -command => [\&letter, 'q'])->pack(-side => 'left');
-	my $R = $game->Button(-text => "R", -command => [\&letter, 'r'])->pack(-side => 'left');
-	my $S = $game->Button(-text => "S", -command => [\&letter, 's'])->pack(-side => 'left');
-	my $T = $game->Button(-text => "T", -command => [\&letter, 't'])->pack(-side => 'left');
-	my $U = $game->Button(-text => "U", -command => [\&letter, 'u'])->pack(-side => 'left');
-	my $V = $game->Button(-text => "V", -command => [\&letter, 'v'])->pack(-side => 'left');
-	my $W = $game->Button(-text => "W", -command => [\&letter, 'w'])->pack(-side => 'left');
-	my $X = $game->Button(-text => "Y", -command => [\&letter, 'y'])->pack(-side => 'left');
-	my $Y = $game->Button(-text => "X", -command => [\&letter, 'x'])->pack(-side => 'left');
-	my $Z = $game->Button(-text => "Z", -command => [\&letter, 'z'])->pack(-side => 'left');
+	$GuessList = $mw->Label(-text => "$guess")->pack();
+
+	$canvas = $mw->Canvas(-relief => "sunken", -background => "blue");
+	$canvas->pack();
+
+
+###############################
+#My Issue is I kind of got the GUI working. You made it so that once the button is clicked, it will push the letter into the $letter.
+#But I cant figure out why it wont run the #CheckLetter sub function
+#Perhaps it is and im stuck on the wrong thing.
+#either way, It doesnt update the counter. So it always just keeps drawing the circle.
+#anythoughts?
+###############################
+	my $A = $mw->Button(-text => "A", -command => [\&letter, 'a'])->pack(-side => 'left');
+	my $B = $mw->Button(-text => "B", -command => [\&letter, 'b'])->pack(-side => 'left');
+	my $C = $mw->Button(-text => "C", -command => [\&letter, 'c'])->pack(-side => 'left');
+	my $D = $mw->Button(-text => "D", -command => [\&letter, 'd'])->pack(-side => 'left');
+	my $E = $mw->Button(-text => "E", -command => [\&letter, 'e'])->pack(-side => 'left');
+	my $F = $mw->Button(-text => "F", -command => [\&letter, 'f'])->pack(-side => 'left');
+	my $G = $mw->Button(-text => "G", -command => [\&letter, 'g'])->pack(-side => 'left');
+	my $H = $mw->Button(-text => "H", -command => [\&letter, 'h'])->pack(-side => 'left');
+	my $I = $mw->Button(-text => "I", -command => [\&letter, 'i'])->pack(-side => 'left');
+	my $J = $mw->Button(-text => "J", -command => [\&letter, 'j'])->pack(-side => 'left');
+	my $K = $mw->Button(-text => "K", -command => [\&letter, 'k'])->pack(-side => 'left');
+	my $L = $mw->Button(-text => "L", -command => [\&letter, 'l'])->pack(-side => 'left');
+	my $M = $mw->Button(-text => "M", -command => [\&letter, 'm'])->pack(-side => 'left');
+	my $N = $mw->Button(-text => "N", -command => [\&letter, 'n'])->pack(-side => 'left');
+	my $O = $mw->Button(-text => "O", -command => [\&letter, 'o'])->pack(-side => 'left');
+	my $P = $mw->Button(-text => "P", -command => [\&letter, 'p'])->pack(-side => 'left');
+	my $Q = $mw->Button(-text => "Q", -command => [\&letter, 'q'])->pack(-side => 'left');
+	my $R = $mw->Button(-text => "R", -command => [\&letter, 'r'])->pack(-side => 'left');
+	my $S = $mw->Button(-text => "S", -command => [\&letter, 's'])->pack(-side => 'left');
+	my $T = $mw->Button(-text => "T", -command => [\&letter, 't'])->pack(-side => 'left');
+	my $U = $mw->Button(-text => "U", -command => [\&letter, 'u'])->pack(-side => 'left');
+	my $V = $mw->Button(-text => "V", -command => [\&letter, 'v'])->pack(-side => 'left');
+	my $W = $mw->Button(-text => "W", -command => [\&letter, 'w'])->pack(-side => 'left');
+	my $X = $mw->Button(-text => "Y", -command => [\&letter, 'y'])->pack(-side => 'left');
+	my $Y = $mw->Button(-text => "X", -command => [\&letter, 'x'])->pack(-side => 'left');
+	my $Z = $mw->Button(-text => "Z", -command => [\&letter, 'z'])->pack(-side => 'left');
+
 }
 
-
-# # Window that pops after you press No
-# sub button2_sub {
-#   my $yesno_button = $mw->messageBox(-message => "Are you sure you don't want to play?",
-#                                         -type => "yesno", -icon => "question");
-#
-#   if ($yesno_button eq "Yes") {
-#     $mw->messageBox(-message => "See you next time.", -type => "ok");
-#     exit;
-#   } else {
-#     $mw->messageBox(-message => "I didn't think so either.", -type => "ok");
-#   }
-# }
 
 # Letter Button takes a letter as a parameter
 sub letter{
@@ -126,6 +102,7 @@ sub CheckLetter {
 		push(@letters_guessed, $letter);
 		@letters_guessed = sort(@letters_guessed);
 		$LettersGuessed -> configure(-text => "Letters Guessed: @letters_guessed");
+
 		for ($i = 0; $i < $word_length; $i++){
 			if ($letter eq substr($word, $i, 1)){
 				substr($guess, $i, 1, $letter);
@@ -135,6 +112,7 @@ sub CheckLetter {
 				$match = 1;
 			}
 		}
+
 		$GuessList -> configure(-text => "$guess");
 		if ($match == 0) {
 			$count++;
@@ -146,13 +124,30 @@ sub CheckLetter {
 	}
 }
 
-
-#! /usr/bin/perl
-# Initiate the dictionary
-#my $size = `wc -l dictionary.txt`;
-my $size = 20;
-#$size =~ s/\D//g;
-print("The size of the file is $size\n");
+sub hangmanDisplay{
+  if ($count == 0) {
+  	$canvas->createRectangle(30, 80, 100, 150, -fill => "yellow");
+  	print "count is $count\n";
+	} elsif ($count == 1) {
+  	$canvas->createRectangle(100, 30, 200, 50, -fill => "cyan");
+  	print "count is $count\n";
+	} elsif ($count == 2) {
+  	$canvas->createRectangle(50, 20, 100, 50, -fill => "cyan");
+  	print "count is $count\n";
+	} elsif ($count == 3) {
+	 	$canvas->createRectangle(50, 20, 100, 50, -fill => "cyan");
+	  print "count is $count\n";
+	} elsif ($count == 4) {
+		$canvas->createRectangle(50, 20, 100, 50, -fill => "cyan");
+		print "count is $count\n";
+	} elsif ($count == 5) {
+		$canvas->createRectangle(50, 20, 100, 50, -fill => "cyan");
+		print "count is $count\n";
+	} elsif ($count == 6) {
+	  	$canvas->createRectangle(50, 20, 100, 50, -fill => "cyan");
+	  	print "count is $count\n";
+	}
+}
 
 # Choose the target word
 $word_index = int(rand($size));
@@ -169,44 +164,6 @@ print($guess, "\n");
 
 
 
-&GUI();
-
-# until ($count == 6 || $win == 1) {
-# 	&hangmanDisplay();
-# 	print("These are the letters you have guessed: @letters_guessed\n");
-# 	print("Please pick a letter: ");
-# 	# $letter = <STDIN>;
-# 	# print("\n");
-# 	# chop($letter);
-# 	if (&check_guessed($letter, join("", @letters_guessed))){
-# 		$game->Label(-text => "These are the letters you have guessed: @letters_guessed")->pack();
-# 	} else {
-# 		push(@letters_guessed, $letter);
-# 		for ($i = 0; $i < $word_length; $i++){
-# 			if ($letter eq $word_list[$i]){
-# 				splice(@guess_list, $i, 1, $letter);
-# 				$match = 1;
-# 			}
-# 		}
-# 		print(@guess_list, "\n");
-# 		if ($match == 0) {
-# 			$count++;
-# 		}
-# 		$match = 0;
-# 		if ($word eq join("", @guess_list)){
-# 			$win = 1;
-# 		}
-# 	}
-# }
-
-if ($win == 1){
-	print("You win!\n");
-} else {
-	&hangmanDisplay();
-	print("You lose!\n");
-	print("The correct word is: ", $word, "\n");
-}
-
 sub fetch_word {
 	open(DICT, "dictionary.txt") or die "Could not open file: $!";
 	my $target = <DICT>;
@@ -217,7 +174,6 @@ sub fetch_word {
 	}
 	$target;
 }
-
 
 # Check if a letter has already been guessed
 sub check_guessed {
@@ -232,102 +188,34 @@ sub check_guessed {
 	$guessed;
 }
 
-sub hangmanDisplay {
-	if ($count == 0){
-		&hangman1Display();
-	} elsif ($count == 1) {
-		&hangman2Display();
-	} elsif ($count == 2) {
-		&hangman3Display();
-	} elsif ($count == 3) {
-		&hangman4Display();
-	} elsif ($count == 4) {
-		&hangman5Display();
-	} elsif ($count == 5) {
-		&hangman6Display();
-	} elsif ($count == 6) {
-		&hangman7Display();
-	}
+	&GUI();
+
+if ($win == 1){
+	print("You win!\n");
+	} else {
+	&hangmanDisplay();
+	print("You lose!\n");
+	print("The correct word is: ", $word, "\n");
 }
 
 
-sub hangman1Display {
-        print "  -------\n";
-        print "  |     |\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "--|----\n";
-			}
-
-sub hangman2Display {
-        print "  -------\n";
-        print "  |     |\n";
-        print "  |     o\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "--|----\n";
-			}
-
-sub hangman3Display {
-        print "  -------\n";
-        print "  |     |\n";
-        print "  |     o\n";
-        print "  |     |\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "--|----\n";
-			}
-
-sub hangman4Display {
-        print "  -------\n";
-        print "  |     |\n";
-        print "  |     o\n";
-        print "  |    \\|\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "--|----\n";
-			}
-
-sub hangman5Display {
-
-        print "  -------\n";
-        print "  |     |\n";
-        print "  |     o\n";
-        print "  |    \\|/\n";
-        print "  |\n";
-        print "  |\n";
-        print "  |\n";
-        print "--|----\n";
-			}
-
-sub hangman6Display {
-
-        print "  -------\n";
-        print "  |     |\n";
-        print "  |     o\n";
-        print "  |    \\|/\n";
-        print "  |    / \n";
-        print "  |\n";
-        print "  |\n";
-        print "--|----\n";
-			}
-
-sub hangman7Display {
-        print "  -------\n";
-        print "  |     |\n";
-        print "  |     x\n";
-        print "  |    \\|/\n";
-        print "  |    / \\\n";
-        print "  |\n";
-        print "  |\n";
-        print "--|----\n";
-	}
-
 	MainLoop;
+
+	#Hangman
+	#
+	# Display punctuation in game.  _ _ _ _ ' _ (Fixed)
+	# Case Insensitive. A, a should be the same. (Fixed)
+
+
+	# Ideas for creative project
+	#
+	# Processing.
+	# EG. When it has this letter at the end of the string, it does this etc.
+	# Variable number of arguments to a function.
+	# Dereferencing
+	# Hashing
+	# Dynamic Arrays
+	# Glue Things together
+	# Variable number arguments
+	# Text Processing
+	# regular expression parsing.
